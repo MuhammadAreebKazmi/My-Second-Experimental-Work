67fd8d222c3f1639d573ca7940a0dcd2a43acfe7
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2019-09-10 02:00:03 -0700

    Initial working
    In this repository, I tried to do understand the meanings and purpose of WriteLine, Readkey, Write and ReadLine.

---
+++ Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myName As String
+        myName = ""
+
+        Dim Counter As Integer
+        Counter = 0
+
+        For Counter = 1 To 3
+            Console.Write("Enter the name: ")
+            myName = Console.ReadLine
+            Console.WriteLine(myName)
+        Next
+        Console.WriteLine("Press any key to continue...")
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
